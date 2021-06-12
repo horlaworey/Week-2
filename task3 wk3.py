d778d78e8b1272be6779c3331fd37dc9644def7c
horlaworey <horlaworey@gmail.com>
2021-06-12 18:40:03 +0100

    Update task3 wk3.py

---
+++ task3 wk3.py
@@ -0,0 +1,17 @@
+class Animal:
+    def __init__(self):
+        pass
+
+
+class Dog(Animal):
+
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+class Cat(Animal):
+
+    def __init__(self):
+        super().__init__()
+        pass
