56c6c90b0873609bbfea58bea786af29b0b39551
horlaworey <horlaworey@gmail.com>
2021-06-13 08:15:41 +0100

    Update task3 wk3.py

--- task3 wk3.py
+++ task3 wk3.py
@@ -1,17 +1,19 @@
 class Animal:
-    def __init__(self):
-        pass
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def speak(self):
+        print(f"I am {self.name} and I am {self.age} years old")
 
 
 class Dog(Animal):
 
-    def __init__(self):
-        super().__init__()
-        pass
+    def __init__(self, name, age):
+        super().__init__(name, age)
 
 
 class Cat(Animal):
 
-    def __init__(self):
-        super().__init__()
-        pass
+    def __init__(self, name, age):
+        super().__init__(name, age)
